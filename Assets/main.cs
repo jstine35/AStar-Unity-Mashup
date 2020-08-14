2d7aea39061d6275b79bd83a71280720d6c64310
Jake Stine <jake.stine@gmail.com>
2020-08-14 15:51:16 -0700

    Fix scale/pos, add more open space tot he map.

--- Assets/main.cs
+++ Assets/main.cs
@@ -11,15 +11,22 @@ public class main : MonoBehaviour
     {
         "+-----------------+",
         "|                 |",
+        "|                 |",
+        "|                 |",
         "| Xxxxxxxxxxxxx   |",
         "|  x              |",
         "|x  x             |",
+        "|x x         X    |",
         "|x x              |",
         "|  x A X          |",
+        "|  XXXXX  X   X   |",
         "| XXXXXX  X       |",
-        "|    x  XX        |",
+        "| XXXXXX  X       |",
+        "|    x  XX     X  |",
         "|   Bx X          |",
         "|xxxxx  XXXXX     |",
+        "|                 |",
+        "|                 |",
         "|                 |",
         "|                 |",
         "+-----------------+",
@@ -29,20 +36,28 @@ public class main : MonoBehaviour
     void Start()
     {
         var plane    = GameObject.Find("Plane");
-        var bounds   = plane.GetComponent<Renderer>().bounds;
+        //var bounds   = plane.GetComponent<Renderer>().bounds;
         var position = plane.GetComponent<Transform>().position;
         var rotation = plane.GetComponent<Transform>().rotation;
 
         //gameObject.GetComponent<Transform>().localScale = scale;
         //gameObject.GetComponent<Transform>().SetPositionAndRotation(position, rotation);
 
-        Vector3 origin = new Vector3(bounds.min.x, 0, bounds.min.z);
-        for (int i=0; i<16; ++i) {
-            var startpos = new Vector3(
-                origin.x + (i & 7) * 3, 0,
-                origin.z + (i % 8) * 3
-            );
-            Instantiate(CubeWallPrefab, startpos, Quaternion.identity, plane.GetComponent<Transform>());
+        Vector3 origin = position; //new Vector3(bounds.min.x, 0, bounds.min.z);
+        int2 map_size = new int2 { x = map[0].Length, y = map.Length };
+
+        for (int y=0; y<map_size.y; ++y) {
+            for (int x=0; x<map_size.x; ++x) {
+                if (map[y][x] == 'A' || map[y][x] == 'B') continue;
+                if (map[y][x] == ' ') continue;
+
+                var scale = CubeWallPrefab.GetComponent<Transform>().localScale;
+                var startpos = new Vector3(
+                    origin.x + (x * scale.x), 0,
+                    origin.z + (y * scale.z)
+                );
+                Instantiate(CubeWallPrefab, startpos, Quaternion.identity, plane.GetComponent<Transform>());
+            }
         }
 
         var curpos = new int2();
