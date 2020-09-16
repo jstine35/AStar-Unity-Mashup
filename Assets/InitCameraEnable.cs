18396a01f0ee0198506ee97f1c36a367d3ac0888
Jake Stine <jake.stine@gmail.com>
2020-09-15 22:52:00 -0700

    Camera: Disable perspective camera by default, fixes bad camera view on startup (randomly)

---
+++ Assets/InitCameraEnable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitCameraEnable : MonoBehaviour
+{
+    public bool Enabled = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Camera>().enabled = Enabled;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
